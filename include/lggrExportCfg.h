1da6a97564b19cbb6c1bc42c0ed30968745308e2
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2024-05-13 15:38:06 +0200

    Avoid DEPRECATED redefinition

--- include/lggrExportCfg.h
+++ include/lggrExportCfg.h
@@ -48,13 +48,15 @@
 #		define LGGR_LOCAL
 #	endif    // LGGR_DLL
 
-#	ifdef __GNUC__
-#		define DEPRECATED __attribute__ ((deprecated))
-#	elif defined(_MSC_VER)
-#		define DEPRECATED __declspec (deprecated)
-#	else
-#		pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-#		define DEPRECATED
+#	ifndef DEPRECATED
+#		ifdef __GNUC__
+#			define DEPRECATED __attribute__ ((deprecated))
+#		elif defined(_MSC_VER)
+#			define DEPRECATED __declspec (deprecated)
+#		else
+#			pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#			define DEPRECATED
+#		endif
 #	endif
 
 #endif    //_LGGR_CFG_H_
